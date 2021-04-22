e17f5c1eb4bdb1791f6ca5b17cdd6907e27e5592
akyllly <79416407+akyllly@users.noreply.github.com>
2021-04-22 16:03:54 +0600

    Update blog.blade.php

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>Document</title>
-    <link rel="stylesheet" href="{{ URL::asset('css/blogcss.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/blogcss.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Josefin Sans' rel='stylesheet'>
 </head>
