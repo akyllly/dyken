86c22f6e305205c619e757e5788c5ee8b387197c
akyllly <79416407+akyllly@users.noreply.github.com>
2021-04-22 16:04:44 +0600

    Update keyboards.blade.php

--- resources/views/keyboards.blade.php
+++ resources/views/keyboards.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Medigo</title>
-  <link rel="stylesheet" href="{{ URL::asset('css/keyboards.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('css/keyboards.css') }}">
   <script src=""></script>
   <!-- <link rel="stylesheet/less" type="text/css" href="/resources/css/style.css"/> -->
   
